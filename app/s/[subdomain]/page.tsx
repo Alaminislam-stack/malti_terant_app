b774478a8fb44bc9983c6d038a2a7ce8a6edb573
Alamin Islam <alamin890213@gmail.com>
2025-07-04 23:07:43 +0600

    Refactor Subdomain component to handle params as a promise and update base URL usage

--- app/s/[subdomain]/page.tsx
+++ app/s/[subdomain]/page.tsx
@@ -5,11 +5,16 @@ import { eq } from "drizzle-orm";
 import Link from "next/link";
 import React from "react";
 
-async function Subdomain({ params }: { params: { subdomain: string } }) {
-  const { subdomain } = params;
+async function Subdomain({
+  params,
+}: {
+  params: Promise<{ subdomain: string }>;
+}) {
+  const { subdomain } = await params;
   const client = await clerkClient();
   const org = await client.organizations.getOrganization({ slug: subdomain });
   const orgId = org?.id;
+  const buseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
   const blogs = await db
     .select()
@@ -17,7 +22,7 @@ async function Subdomain({ params }: { params: { subdomain: string } }) {
     .where(eq(blogTable.orgId, orgId));
 
   return (
-    <div className="py-10 lg:py-[50px] px-10 lg:px-[200px] h-screen">
+    <div className=" py-10 lg:py-[50px] px-10 lg:px-[200px] h-screen">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {[...blogs]
           .sort((a, b) => b.id.localeCompare(a.id))
@@ -29,7 +34,7 @@ async function Subdomain({ params }: { params: { subdomain: string } }) {
                   ? blog.body.slice(0, 100) + "..."
                   : blog.body}
               </p>
-              <Link href={`/s/${subdomain}/subsiteblog/${blog.id}`}>
+              <Link href={`${buseUrl}/s/${subdomain}/subsiteblog/${blog.id}`}>
                 <span className="text-blue-600 hover:text-blue-300 cursor-pointer">
                   See More
                 </span>
