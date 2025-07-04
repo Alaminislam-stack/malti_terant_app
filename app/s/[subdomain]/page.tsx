e2f4a5126ce1be3cd7803eac1275f3bb6ee999a4
Alamin Islam <alamin890213@gmail.com>
2025-07-04 23:02:12 +0600

    Remove unused base URL variable and clean up JSX formatting in Subdomain component

--- app/s/[subdomain]/page.tsx
+++ app/s/[subdomain]/page.tsx
@@ -10,7 +10,6 @@ async function Subdomain({ params }: { params: { subdomain: string } }) {
   const client = await clerkClient();
   const org = await client.organizations.getOrganization({ slug: subdomain });
   const orgId = org?.id;
-  const buseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
   const blogs = await db
     .select()
@@ -18,7 +17,7 @@ async function Subdomain({ params }: { params: { subdomain: string } }) {
     .where(eq(blogTable.orgId, orgId));
 
   return (
-    <div className=" py-10 lg:py-[50px] px-10 lg:px-[200px] h-screen">
+    <div className="py-10 lg:py-[50px] px-10 lg:px-[200px] h-screen">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {[...blogs]
           .sort((a, b) => b.id.localeCompare(a.id))
@@ -30,7 +29,7 @@ async function Subdomain({ params }: { params: { subdomain: string } }) {
                   ? blog.body.slice(0, 100) + "..."
                   : blog.body}
               </p>
-              <Link href={`${buseUrl}/s/${subdomain}/subsiteblog/${blog.id}`}>
+              <Link href={`/s/${subdomain}/subsiteblog/${blog.id}`}>
                 <span className="text-blue-600 hover:text-blue-300 cursor-pointer">
                   See More
                 </span>
